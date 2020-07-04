7afc17f3be8486da903f99cbf3c8fdef230c47ac
Alex <alex2005.uy@gmail.com>
2020-07-04 18:42:54 -0300

    se pone el sevricio de mensajerias en el header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UsuarioService } from '../../servicios/usuario/usuario.service';
 import { Usuario } from '../../modelos/usuario.model';
 import { Router } from '@angular/router';
 import { WebsocketService } from 'src/app/servicios/websocket/websocket.service';
+import { Subscription } from 'rxjs';
+import { ChatService } from 'src/app/servicios/chat/chat.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styles: []
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   usuario: Usuario;
 
-  constructor(public usuariosrv: UsuarioService, public router: Router, public wsocket: WebsocketService) {
+  mensajes: any [] = [];
+  texto = "";
+  mensajesSuscripcion: Subscription;
+  elemento: HTMLElement;
+  mensajes_sinleer = false;
+
+  constructor(public usuariosrv: UsuarioService, public router: Router, public wsocket: WebsocketService, public chat: ChatService) {
 
     
    }
@@ -23,6 +31,32 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
 
     this.usuario = this.usuariosrv.usuario;
+
+
+    /* obtengo la ereferencia a la caja de chat ya que voy a hacer un scroll automatico */
+    this.elemento = document.getElementById('chat-mensajes');
+
+    /* me suscribo al observable que va a tener los mensajes nuevos */
+    this.mensajesSuscripcion =this.chat.escucharMensajes().subscribe((msg:any) => {
+
+      this.mensajes.push(msg);
+
+      /* si no aoy yo entonces hay mensajes sin leer */
+      if (msg.de != this.usuario.nombre){
+        this.mensajes_sinleer = true;
+
+      }
+    
+
+      /* le pongo un timeout minimo al scroll por que necesito que espere a que este el mensaje rendrizado */
+      setTimeout( ()=> {
+
+
+        this.elemento.scrollTop = this.elemento.scrollHeight;
+        
+      }, 50);
+
+    });
   }
 
 
@@ -35,6 +69,19 @@ export class HeaderComponent implements OnInit {
 
     this.router.navigate(['/busqueda/'+t2]);
 
+  }
+
+   /* cuando salga del chat destruyo la suscripcion */
+   ngOnDestroy(){
+    this.mensajesSuscripcion.unsubscribe();
+  }
+
+  enviar(){
+
+    this.chat.enviarMensaje(this.texto);
+ 
+
+    this.texto = '';
   }
 
 
